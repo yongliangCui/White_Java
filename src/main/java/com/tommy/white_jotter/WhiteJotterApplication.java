8f10211558251ef41d22cb8b38d74db3e474f9de
Tommy <425274609@qq.com>
2022-06-10 14:54:54 +0800

    开启swagger

--- src/main/java/com/tommy/white_jotter/WhiteJotterApplication.java
+++ src/main/java/com/tommy/white_jotter/WhiteJotterApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableCaching
 @SpringBootApplication
 @Import(ShiroConfiguration.class)
 @EnableTransactionManagement
+@EnableSwagger2
 public class WhiteJotterApplication {
     public static void main(String[] args) {
         SpringApplication.run(WhiteJotterApplication.class, args);
